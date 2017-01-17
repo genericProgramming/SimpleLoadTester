59e23cd8dddb387c40ac2f774182950d418a97a4
ben <ben@laptop>
2017-01-17 17:44:49 -0600

    rough outline of the aggregator and monitor

--- components/responseAggregator.go
+++ components/responseAggregator.go
@@ -1,5 +1,14 @@
 package components
 
+import metrics "github.com/rcrowley/go-metrics"
+
+const (
+	sampleSize      int     = 1028
+	sampleAlpha     float64 = 0.015
+	responseTimeKey string  = "ResponseTimeMetric"
+	errorCodeKey    string  = "ErrorCodesGauge"
+)
+
 /**
 TODO
 Needed metrics are
@@ -10,15 +19,43 @@ Needed metrics are
 
 // ResponseAggregator is a Singleton that captures and exposes the metric data
 type ResponseAggregator interface {
-	Start()
+	ListenAndAggregate(<-chan RequestResult)
 }
 
 type GoMetricBasedAggregator struct {
-	config *Config
+	config                *Config
+	responseTimeHistogram metrics.Histogram
+	errorCodesGauge       metrics.Gauge
 }
 
-func (aggregator *GoMetricBasedAggregator) Start() {
-	// add responseTimeMetrics
-	// add status code Gauge
-	// TODO figure out what life will look like for
+// ListenAndAggregate sets up metrics and listens on the channel for completed requests
+// aggregating accordingly
+func (aggregator *GoMetricBasedAggregator) ListenAndAggregate(results <-chan RequestResult) {
+	aggregator.setupMetrics()
+
+	go aggregator.aggregateResults(results)
+}
+
+func (aggregator *GoMetricBasedAggregator) setupMetrics() {
+	config := aggregator.config
+	aggregator.responseTimeHistogram = getResponseTimMetricsHistogram(config)
+	aggregator.errorCodesGauge = getErrorCodeGauge(config)
+	// TODO figure out what life will look like for other metrics
+}
+
+func getResponseTimMetricsHistogram(config *Config) metrics.Histogram {
+	s := metrics.NewExpDecaySample(sampleSize, sampleAlpha) // or metrics.NewUniformSample(1028)
+	h := metrics.NewHistogram(s)
+	metrics.Register(responseTimeKey, h)
+	return h
+}
+
+func getErrorCodeGauge(config *Config) metrics.Gauge {
+	g := metrics.NewGauge()
+	metrics.Register(errorCodeKey, g)
+	return g
+}
+
+func (aggregator *GoMetricBasedAggregator) aggregateResults(results <-chan RequestResult) {
+
 }
--- monitor.go
+++ monitor.go
@@ -4,17 +4,32 @@ import "github.com/genericProgramming/simpleLoadTester/components"
 import "log"
 
 type Monitor interface {
-	Start()
+	IsApiHealthy()
+	RampUp()
+	RampDown()
 }
 
 type StairCaseMonitor struct {
-	config *components.Config
-	engine components.Engine
+	config      *components.Config
+	engine      components.Engine
+	aggregator  components.ResponseAggregator
+	currentRate components.Rate
 }
 
-func (monitor *StairCaseMonitor) Start() {
-	log.Panicln("Lol -- probs should implement this")
+func RunMonitor(monitor Monitor) {
+	go func(){
+		for {
+			performWork()
+			sleep()
+		}
+	}()
 }
+
+func isApiHealthy(aggregator components.ResponseAggregator) Data{
+	log.Panic("NYI")
+}
+
+func 
 
 func NewMonitor(config *components.Config) Monitor {
 	return &StairCaseMonitor{
